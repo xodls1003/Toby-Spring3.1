ada01cf36a751f02800b85115cb37817a09e9fe8
user <user@211.217.115.245>
2021-07-21 13:48:58 +0900

    spring from pojo

---
+++ src/springbook/user/dao/SimpleConnectionMaker.java
@@ -0,0 +1,19 @@
+package springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SimpleConnectionMaker {	
+
+	public Connection makeNewConnection() throws ClassNotFoundException,SQLException
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection c = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/toby","root","rlaxhgus33!");
+			return c;
+		
+		
+	}
+	
+}
